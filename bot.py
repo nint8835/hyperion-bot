ffd8ea9225655db29c3a24cf320609f9834ec7c1
Riley Flynn <riley@rileyflynn.me>
2022-06-08 20:11:17 -0230

    Delete existing bot, begin new bot

--- bot.py
+++ bot.py
@@ -1,21 +1,47 @@
-import os
-from typing import Any, Dict
-
-from discord import Embed
-from discord.ext import commands
-from dotenv import load_dotenv
-from requests import Session
-
-from cogs.currency import Currency
-from cogs.debug import Debug
-
-load_dotenv()
+import discord
+import requests
+from discord import app_commands
+from pydantic import BaseSettings
 
 
-bot = commands.Bot(command_prefix="hyp!", description="Hyperion Test Bot")
+class Config(BaseSettings):
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+    discord_token: str
+    hyperion_endpoint: str
+    hyperion_integration_token: str
+
+    testing_guild_id: str = "497544520695808000"
 
 
-bot.add_cog(Debug(bot))
-bot.add_cog(Currency(bot))
+config = Config()
+guild = discord.Object(config.testing_guild_id)
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+session = requests.session()
+session.headers.update(
+    {
+        "Authorization": f"Bearer {config.hyperion_integration_token}",
+        "User-Agent": "Hyperion Bot",
+    }
+)
+
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(guild=guild)
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message("Hello, world!")
+
+
+@client.event
+async def on_ready():
+    print("Bot ready, syncing commands")
+    await tree.sync(guild=guild)
+
+
+client.run(config.discord_token)
